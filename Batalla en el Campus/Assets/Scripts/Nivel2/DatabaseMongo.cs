f859c695f6be4621be2c4b58357eb35005817fcb
Lizbeth <liz.p.salinas23123@gmial.com>
2023-11-17 02:10:16 -0600

    Se agrega avance, (Guardar nombre del jugador)

--- Batalla en el Campus/Assets/Scripts/Nivel2/DatabaseMongo.cs
+++ Batalla en el Campus/Assets/Scripts/Nivel2/DatabaseMongo.cs
@@ -19,6 +19,8 @@ public class DatabaseMongo : MonoBehaviour
 
     public void SaveScore(string playerName, int playerScore)
     {
+      
+
         var scoreDocument = new BsonDocument
         {
             { "playerName", playerName },
